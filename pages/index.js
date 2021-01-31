07636f1982452ee39726b102efe886709f73ffb1
Shimon Wosner <31377730+shwosner@users.noreply.github.com>
2021-01-31 12:54:13 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,7 @@ export default function Home() {
 
           <a href="https://nextjs.org/learn" className={styles.card}>
             <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+            <p>Learn about Next.js in an interactive course with quizzes 123!</p>
           </a>
 
           <a
