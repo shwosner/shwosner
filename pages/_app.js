ce92f74523ce7de5cb08be643d29515acc4627bd
Shimon Wosner <shwosner@gmail.com>
2021-02-04 02:36:34 -0500

    Added meta og tags

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,22 @@
 import "../styles/globals.css";
 import SimpleReactLightbox from "simple-react-lightbox";
-
+import Head from "next/head";
 function MyApp({ Component, pageProps }) {
   return (
-    <SimpleReactLightbox>
-      <Component {...pageProps} />
-    </SimpleReactLightbox>
+    <div>
+      <Head>
+        <meta property="og:title" content="Shimon Wosner" />
+        <meta property="og:description" content="Shimomn's web dev porfolio" />
+        <meta property="og:image" content="http://shimon-wosner.vercel.app/" />
+        <meta
+          property="og:url"
+          content="http://shimon-wosner.vercel.app/shimon_compressed.jpg"
+        />
+      </Head>
+      <SimpleReactLightbox>
+        <Component {...pageProps} />
+      </SimpleReactLightbox>
+    </div>
   );
 }
 
